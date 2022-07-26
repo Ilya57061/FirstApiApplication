6965aa83f5734f9eab194c0c3367a7e7cc476494
Ilya57061 <81263807+Ilya57061@users.noreply.github.com>
2022-07-26 20:27:08 +0300

    Update ApplicationContext.cs

--- FirstApiApplication/Models/ApplicationContext.cs
+++ FirstApiApplication/Models/ApplicationContext.cs
@@ -5,7 +5,7 @@ namespace FirstApiApplication.Models
     public class ApplicationContext:DbContext
     {
         public DbSet<User> Users { get; set; } = null!;
-        public DbSet<Company>? Сompanies { get; set; } = null!;
+        public DbSet<Company> Сompanies { get; set; } = null!;
         public ApplicationContext(DbContextOptions<ApplicationContext> options):base(options)
         {
             Database.EnsureDeleted();
